a756057c1dfc983a37116a6d31a2f108219cec9d
Danniel Cristie <dannielcristie@gmail.com>
2023-07-17 11:52:46 -0300

    Order, Instant, ISO 8601 - PART 2

---
+++ src/main/java/com/springbootcourse/course/repositories/OrderRepository.java
@@ -0,0 +1,8 @@
+package com.springbootcourse.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.springbootcourse.course.entities.Order;
+
+public interface OrderRepository extends JpaRepository<Order, Long> {
+}
